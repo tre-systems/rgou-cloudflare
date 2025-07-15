a05dc47ed549c334ebcaef6aae3a43d65f67d6d3
rgilks <rob.gilks@gmail.com>
2025-07-15 22:53:19 +0100

    Update generated service worker

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '1752616287615-local';
+const CACHE_VERSION = '1752616334199-local';
 const CACHE_NAME = `royal-game-of-ur-${CACHE_VERSION}`;
 const OFFLINE_URL = '/offline';
 
